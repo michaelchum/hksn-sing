d2bbaa39419877d1c99da2d848fc57ca6a64b3d5
Michael Ho <michaelhochum@gmail.com>
2014-10-21 21:29:26 -0400

    Added cache

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = express();
 var compression = require('compression');
 
 app.use(compression());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', { maxAge: 86400000 /* 1d */ }));
 
 app.listen(process.env.PORT || 3000);
