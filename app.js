e8723c527deb9654baa51156820721dfcca05d56
Michael Ho <michaelhochum@gmail.com>
2014-10-21 20:27:19 -0400

    Compressed images

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var compression = require('compression');
 
+app.use(compression());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
